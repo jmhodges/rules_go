1fee69e56cd6f00a882f5a436d2855c94ee5a04c
Jeff Hodges <jeff@somethingsimilar.com>
2016-10-22 20:36:19 -0700

    add cgo support to gazelle
    This adds cgo support to gazelle.
    
    A cgo_library rule called cgo_default_library is created if there are
    any CFiles, HFiles, or CgoFiles in the package. If there are no GoFiles,
    a go_library without srcs is added but with library set to
    cgo_default_library.
    
    If a cgo_library was created, we move the assembly SFiles over to the
    cgo_library to make sure they are in scope for that code.
    
    This change also makes sure we include the Go-expected capital S
    extension of assembly files.
    
    This change depends on the main sandwich PR #164 and the cgo_library in
    root of repo workaround #160.

--- go/tools/gazelle/rules/generator.go
+++ go/tools/gazelle/rules/generator.go
@@ -91,15 +91,25 @@ func (g *generator) Generate(rel string, pkg *build.Package) ([]*bzl.Rule, error
 		rules = append(rules, p)
 	}
 
-	libName := defaultLibName
-	r, err := g.generateLib(rel, libName, pkg)
+	cgoLibrary := ""
+	if len(pkg.CgoFiles) != 0 || len(pkg.CFiles) != 0 || len(pkg.HFiles) != 0 {
+		cgoLibrary = "cgo_default_library"
+		r, err := g.generateCgoCLib(rel, cgoLibrary, pkg)
+		if err != nil {
+			return nil, err
+		}
+		rules = append(rules, r)
+	}
+
+	library := defaultLibName
+	r, err := g.generateLib(rel, library, pkg, cgoLibrary)
 	if err != nil {
 		return nil, err
 	}
 	rules = append(rules, r)
 
 	if pkg.IsCommand() {
-		r, err := g.generateBin(rel, libName, pkg)
+		r, err := g.generateBin(rel, library, pkg)
 		if err != nil {
 			return nil, err
 		}
@@ -115,7 +125,7 @@ func (g *generator) Generate(rel string, pkg *build.Package) ([]*bzl.Rule, error
 	}
 
 	if len(pkg.TestGoFiles) > 0 {
-		t, err := g.generateTest(rel, pkg, r.AttrString("name"))
+		t, err := g.generateTest(rel, pkg, library)
 		if err != nil {
 			return nil, err
 		}
@@ -123,7 +133,7 @@ func (g *generator) Generate(rel string, pkg *build.Package) ([]*bzl.Rule, error
 	}
 
 	if len(pkg.XTestGoFiles) > 0 {
-		t, err := g.generateXTest(rel, pkg, r.AttrString("name"))
+		t, err := g.generateXTest(rel, pkg, library)
 		if err != nil {
 			return nil, err
 		}
@@ -148,7 +158,7 @@ func (g *generator) generateBin(rel, library string, pkg *build.Package) (*bzl.R
 
 }
 
-func (g *generator) generateLib(rel, name string, pkg *build.Package) (*bzl.Rule, error) {
+func (g *generator) generateLib(rel, name string, pkg *build.Package, cgoName string) (*bzl.Rule, error) {
 	kind := "go_library"
 
 	visibility := "//visibility:public"
@@ -156,18 +166,63 @@ func (g *generator) generateLib(rel, name string, pkg *build.Package) (*bzl.Rule
 	if pkg.IsCommand() {
 		visibility = "//visibility:private"
 	}
-	visibility = checkInternalVisibility(rel, visibility)
 
 	attrs := []keyvalue{
 		{key: "name", value: name},
-		{key: "srcs", value: append(pkg.GoFiles, pkg.SFiles...)},
-		{key: "visibility", value: []string{visibility}},
 	}
+
+	if len(cgoName) == 0 {
+		srcs := append([]string{}, pkg.GoFiles...)
+		srcs = append(srcs, pkg.SFiles...)
+		attrs = append(attrs, keyvalue{key: "srcs", value: srcs})
+	} else {
+		// go_library gets mad when an empty slice is passed in, but handles not
+		// being set at all just fine when "library" is set.
+		if len(pkg.GoFiles) != 0 {
+			attrs = append(attrs, keyvalue{key: "srcs", value: pkg.GoFiles})
+		}
+		attrs = append(attrs, keyvalue{key: "library", value: ":" + cgoName})
+	}
+
+	visibility = checkInternalVisibility(rel, visibility)
+	attrs = append(attrs, keyvalue{key: "visibility", value: []string{visibility}})
 
 	deps, err := g.dependencies(pkg.Imports, rel)
 	if err != nil {
 		return nil, err
 	}
+
+	if len(deps) > 0 {
+		attrs = append(attrs, keyvalue{key: "deps", value: deps})
+	}
+
+	return newRule(kind, nil, attrs)
+}
+
+// Generates a cgo_library rule for C/C++ code.
+func (g *generator) generateCgoCLib(rel, name string, pkg *build.Package) (*bzl.Rule, error) {
+	kind := "cgo_library"
+
+	attrs := []keyvalue{
+		{key: "name", value: name},
+	}
+
+	srcs := append([]string{}, pkg.CgoFiles...)
+	srcs = append(srcs, pkg.CFiles...)
+	srcs = append(srcs, pkg.CXXFiles...)
+	srcs = append(srcs, pkg.HFiles...)
+	srcs = append(srcs, pkg.SFiles...)
+	attrs = append(attrs, keyvalue{key: "srcs", value: srcs})
+
+	visibility := "//visibility:private"
+	visibility = checkInternalVisibility(rel, visibility)
+	attrs = append(attrs, keyvalue{key: "visibility", value: []string{visibility}})
+
+	deps, err := g.dependencies(pkg.Imports, rel)
+	if err != nil {
+		return nil, err
+	}
+
 	if len(deps) > 0 {
 		attrs = append(attrs, keyvalue{key: "deps", value: deps})
 	}
---
+++ go/tools/gazelle/testdata/repo/allcgolib/foo.c
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+int64_t callC() {
+  return 42;
+}
