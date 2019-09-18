5e70c9c3bb1a5b1636d9bde7a7ce3756b343e9ad
Jeff Hodges <jeff@somethingsimilar.com>
2019-09-18 00:08:49 -0700

    add failing go_bazel_test for gopackagesdriver

---
+++ tests/core/gopackages/gopackages_test.go
@@ -0,0 +1,78 @@
+package gopackages_test
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/bazelbuild/rules_go/go/tools/bazel_testing"
+	"golang.org/x/tools/go/packages"
+)
+
+// This is to get the platform-specific path to gopackagesdriver in the
+// encompassing rules_go's bazel-bin. See the gopackages_test target in
+// BUILD.bazel.
+var goPkgDriverPath = flag.String("goPkgDriverPath", "", "path to the gopackagesdriver binary")
+
+func TestMain(m *testing.M) {
+	bazel_testing.TestMain(m, bazel_testing.Args{
+		Main: `
+-- BUILD.bazel --
+load("@io_bazel_rules_go//go:def.bzl", "go_library")
+
+go_library(
+	name = "hello",
+	srcs = ["hello.go"],
+	importpath = "fakeimportpath/hello",
+	visibility = ["//visibility:public"],
+)
+-- hello.go --
+package hello
+
+import "fmt"
+
+func A() string { return fmt.Sprintf("A is", 12) }
+`,
+	})
+}
+
+func Test(t *testing.T) {
+	// check we can actually build :hello
+	args := []string{
+		"build",
+		"//:hello",
+	}
+	if err := bazel_testing.RunBazel(args...); err != nil {
+		t.Fatalf("unable to build //:hello normally: %s", err)
+	}
+	driverPath, err := getDriverPath()
+	if err != nil {
+		t.Fatal(err)
+	}
+	os.Setenv("GOPACKAGESDRIVER", driverPath)
+	pkgs, err := packages.Load(nil, "//:hello")
+	if err != nil {
+		t.Fatalf("unable to packages.Load: %s", err)
+	}
+	if len(pkgs) < 1 {
+		t.Fatalf("no packages returned")
+	}
+	if len(pkgs) != 1 {
+		t.Errorf("too many packages returned: want 1, got %d", len(pkgs))
+	}
+}
+
+func getDriverPath() (string, error) {
+	if *goPkgDriverPath == "" {
+		return "", errors.New("-goPkgDriverPath arg was not passed to the test binary")
+	}
+	driverPath := os.Getenv("PWD") + "/" + *goPkgDriverPath
+	f, err := os.Open(driverPath)
+	if err != nil {
+		return "", fmt.Errorf("gopackagesdriver binary couldn't be opened at -goPkgDriverPath %v: %w", *goPkgDriverPath, err)
+	}
+	f.Close()
+	return driverPath, nil
+}
