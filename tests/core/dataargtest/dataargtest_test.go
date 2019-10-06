da844f6575f1998320b1ab201788a5f552779e9f
Jeff Hodges <jeff@somethingsimilar.com>
2019-10-06 10:41:57 -0700

    repro for #2233
    run `bazel test //tests/core/dataargtest:dataargtest_test --test_output=errors`

---
+++ tests/core/dataargtest/dataargtest_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/bazelbuild/rules_go/go/tools/bazel_testing"
+)
+
+var md5sumFlag = flag.String("md5Path", "", "")
+
+func TestMain(m *testing.M) {
+	bazel_testing.TestMain(m, bazel_testing.Args{
+		Main: `
+-- BUILD.bazel --
+load("@io_bazel_rules_go//go:def.bzl", "go_library")
+
+go_library(
+	name = "hello",
+	srcs = ["hello.go"],
+	importpath = "fakeimportpath/hello",
+	visibility = ["//visibility:public"],
+)
+-- hello.go --
+package hello
+
+import "fmt"
+
+func A() string { return fmt.Sprintf("hello is %d", 12) }
+`,
+	})
+}
+
+func TestGoldenPath(t *testing.T) {
+	if err := bazel_testing.RunBazel("build", "//:hello"); err != nil {
+		t.Fatalf("unable to build //:hello normally: %s", err)
+	}
+}
